4b54a4b5e8350fc19cc26259c654fdbecb3b8b13
jon <Jonecton@gmail.com>
2023-09-26 16:09:18 -0500

    improved OR and OCR, added sqlite db and routes for testing

--- app.py
+++ app.py
@@ -1,47 +1,124 @@
-from flask import Flask
+from flask import Flask, render_template, Response, request
 import cv2
 import pytesseract
+import datetime
+import os
+import sqlite3
+
+conn = sqlite3.connect('student.db')
+c = conn.cursor()
+
+# Create the student table
+c.execute('''CREATE TABLE IF NOT EXISTS students (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                parents TEXT,
+                vehicles TEXT,
+                grade INTEGER,
+                teacher TEXT,
+                classroom INTEGER,
+                license_plate TEXT,
+                released INTEGER DEFAULT 0
+            )''')
+
+conn.commit()
+conn.close()
+
+app = Flask(__name__)
+
+if not os.path.exists('captured_images'):
+    os.makedirs('captured_images')
 
 cap = cv2.VideoCapture(0)
 
-# Define minimum and maximum dimensions for license plates
+# Define min and max dimensions for license plates
 min_width = 80
 min_height = 40
 max_width = 400
 max_height = 200
 
-while True:
-    ret, frame = cap.read()
+# Define the width and height frame of the cameraview
+desired_width = 640
+desired_height = 480
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    edges = cv2.Canny(gray, 100, 200)
-    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+def generate_frames():
+    while True:
+        ret, frame = cap.read()
 
-    for contour in contours:
-        x, y, w, h = cv2.boundingRect(contour)
-        if w > min_width and h > min_height and w < max_width and h < max_height:
-            plate_region = frame[y:y + h, x:x + w]
-            plate_text = pytesseract.image_to_string(plate_region, config='--psm 6')
+        # Resize the frame to the desired width and height
+        frame = cv2.resize(frame, (desired_width, desired_height))
 
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
-            cv2.putText(frame, 'License Plate: ' + plate_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                        (0, 0, 0), 2)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        _, thresholded = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.imshow('License Plate Detection', frame)
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
+            if w > min_width and h > min_height and w < max_width and h < max_height:
+                plate_region = thresholded[y:y + h, x:x + w]
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
-        break
+                plate_text = pytesseract.image_to_string(plate_region,
+                                                         config='--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 
-cap.release()
-cv2.destroyAllWindows()
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 0), 2)
+                cv2.putText(frame, 'License Plate: ' + plate_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                            (0, 0, 0), 2)
 
-app = Flask(__name__)
+                # Check if the recognized license plate is in the database and not released
+                conn = sqlite3.connect('student.db')
+                c = conn.cursor()
+                c.execute('SELECT * FROM students WHERE license_plate = ? AND released = 0', (plate_text,))
+                student = c.fetchone()
 
+                if student:
+                    print(f"License Plate Recognized. Student: {student[1]}, Classroom: {student[6]}")
+                    # Mark the student as released in the database
+                    c.execute('UPDATE students SET released = 1 WHERE id = ?', (student[0],))
+                    conn.commit()
+                conn.close()
+
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/student/<int:student_id>')
+def student_info(student_id):
+    conn = sqlite3.connect('student.db')
+    c = conn.cursor()
+
+    # Retrieve student information by ID from the database
+    c.execute('SELECT * FROM students WHERE id = ?', (student_id,))
+    student = c.fetchone()
+
+    conn.close()
+
+    return render_template('student_info.html', student=student)
+
+@app.route('/release/<int:classroom>')
+def release_students(classroom):
+    conn = sqlite3.connect('student.db')
+    c = conn.cursor()
+
+    # Retrieve students in the specified classroom who have not been released
+    c.execute('SELECT * FROM students WHERE classroom = ? AND released = 0', (classroom,))
+    students = c.fetchall()
+
+    conn.close()
+
+    return render_template('release_students.html', students=students, classroom=classroom)
+
+@app.route('/cameraview')
+def cameraview():
+    return render_template('cameraview.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
